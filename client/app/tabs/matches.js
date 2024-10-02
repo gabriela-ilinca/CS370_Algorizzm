abd51ca435ce1dc12554421db4c3221fdef4e75b
jysaucy <alice.song@emory.edu>
2024-10-02 14:34:12 -0400

    Added Liked Matched buttons

--- client/app/tabs/matches.js
+++ client/app/tabs/matches.js
@@ -1,4 +1,4 @@
-import { Text, View, SafeAreaView } from 'react-native';
+import { Text, View, Button, SafeAreaView, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { Stack, useRouter } from 'expo-router';
 
@@ -9,8 +9,29 @@ const Matches = () => {
                 options={{ 
                     headerShown: false,
                     headerTitle:"",
+                    headerStyle: {
+                        backgroundColor: '#111111',
+                    },
                 }}
             />
+            <View style ={{flex: 1, justifyContent: 'center', alignItems: 'center',}}>
+                <View style = {{flexDirection:'row', justifyContent:'space-evenly', position: 'absolute', width:'100%',top:30}}>
+                    <TouchableOpacity onPress={() => {}}>
+                        <Text style={{color:'#fff', fontSize: 24, fontWeight: 'bold'}}>
+                            LIKED
+                        </Text>
+
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => {}}>
+                        <Text style={{color:'#fff', fontSize: 24, fontWeight: 'bold'}}>
+                            MATCHED
+                        </Text>
+
+                    </TouchableOpacity>
+                    
+                </View> 
+        
+            </View>
                 <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor:'#111111'  }}>
                     <Text style={{color: 'white'}}>Matches Screen</Text>
                 </View>
